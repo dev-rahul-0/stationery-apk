3b2ef4074acc92e7cece717f24668b4ca4e7cfcc
dev-rahul-0 <rs650417@gmail.com>
2023-12-29 22:21:15 +0530

    update

--- lib/note_database.dart
+++ lib/note_database.dart
@@ -4,6 +4,9 @@ class NoteDatabase {
   static late Isar isar;
   static Future<void> initialize() async{
     final dir = await getApplicationDocumentsDirectory();
-    isar = await Isar.open(schemas, directory: directory)
+    isar = await Isar.open(
+        [NoteSchema],
+        directory: dir.path );
   }
+  final List<Note> currentNotes = [];
 }
