33f41e5a88856e9468e10feefeb7143310e3775a
dev-rahul-0 <rs650417@gmail.com>
2023-12-29 22:15:31 +0530

    update

---
+++ lib/note_database.dart
@@ -0,0 +1,9 @@
+import 'package:isar/isar.dart';
+import 'package:path_provider/path_provider.dart';
+class NoteDatabase {
+  static late Isar isar;
+  static Future<void> initialize() async{
+    final dir = await getApplicationDocumentsDirectory();
+    isar = await Isar.open(schemas, directory: directory)
+  }
+}
