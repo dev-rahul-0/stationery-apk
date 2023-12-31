baed9d170d25d00a484839816b5c5716157b4edf
dev-rahul-0 <rs650417@gmail.com>
2023-12-31 19:05:24 +0530

    update

--- lib/notepage.dart
+++ lib/notepage.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class NotePage extends StatefulWidget {
   const NotePage({super.key});
 
@@ -12,12 +14,46 @@ class _NotePageState extends State<NotePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.redAccent,
-        title: Text('Stationary Items'),
+        title: const Text('Stationary Items'),
         centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 10,right: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+                flex: 8,
+                child: StreamBuilder(
+                  stream: item.snapshots(),
+                  builder:
+                      (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                    if (streamSnapshot.hasData) {
+                      return ListView.builder(
+                          itemCount: streamSnapshot.data!.docs.length,
+                          itemBuilder: (context, index) {
+                            final DocumentSnapshot documentSnapshot =
+                                streamSnapshot.data!.docs[index];
+                            return Card(
+                              margin: const EdgeInsets.all(10),
+                              child: ListTile(
+                                title: Text(
+                                  documentSnapshot['name'],
+                                  style: const TextStyle(
+                                      fontWeight: FontWeight.w600, fontSize: 17),
+                                ),
+                              ),
+                            );
+                          });
+                    }
+                  },
+                ))
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.redAccent,
-        onPressed: (){},
+        onPressed: () {},
         child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
